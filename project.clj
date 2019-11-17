312d7b77d467f93efff1a31cad2e1173c6b45a1d
Dominik Berger <dmnkbrgr@gmail.com>
2019-11-17 22:55:51 +0100

    update leiningen dependencies

--- project.clj
+++ project.clj
@@ -3,5 +3,9 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.0"]]
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [crouton "0.1.2"]
+                 [org.clojars.daydream/webmine "0.1.11-SNAPSHOT"]
+                 [enlive "1.1.6"]
+                 [etaoin "0.3.5"]]
   :repl-options {:init-ns amazon-wishlist.core})
