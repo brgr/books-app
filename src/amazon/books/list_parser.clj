a59d53e9e0c357ebab5b183f9900404bbb697939
Dominik Berger <dmnkbrgr@gmail.com>
2019-11-21 23:09:40 +0100

    clean up the html (wishlist) parser

--- src/amazon/books/list_parser.clj
+++ src/amazon/books/list_parser.clj
@@ -1,41 +1,9 @@
 (ns amazon.books.list-parser
-  (:require [crouton.html :as html])
   (:import [org.jsoup Jsoup]))
 
-; https://github.com/igrishaev/etaoin#getting-stated
 
-;(println (System/getProperty "user.dir"))
-(def wishlist_page (html/parse "../../../resources/wishlist_page.html"))
-(def wishlist_html_items_file "itemNames_reddit.html")
-(def wishlist_html_items (slurp wishlist_html_items_file))
-
-(def whole_html (slurp "whole_html.html"))
-
-;(println wishlist_page)
-
-(defn has_correct_attrs? [ele]
-  (contains? ele :attrs))
-
-;(spit "output.clj" wishlist_page)
-
-; The list items are in these elements: "{:tag :span, :attrs {:class "a-list-item"}, :content ..." in :content!
-; Although they give a little bit too many results. They need to be filtered again.
-
-(defn find-all-nested
-  [m k]
-  (->> (tree-seq map? vals m)
-       (filter map?)
-       (keep k)))
-
-(def items (find-all-nested wishlist_page :tag))
-
-;; with JSoup
-; for selectors, see this reddit post:
+; For selectors, see this reddit post:
 ; https://old.reddit.com/r/amazon/comments/7paenb/amazon_wishlist_web_scraper/
-
-(def soup (Jsoup/parse whole_html))
-
-(def wishlist_items (.select soup "li.g-item-sortable"))
 
 (defn get_data [wishlist_item]
   (let [id (.attr wishlist_item "data-itemid")
@@ -47,5 +15,13 @@
         price (.text (.select wishlist_item ".itemUsedAndNewPrice"))]
     {:id id :title title :author author :url url :thumbnail thumbnail :itemAddedDate itemAddedDate :price price}))
 
-(println (mapv get_data wishlist_items))
+(defn load_book_data_from_wishlist [whole_wishlist_html]
+  (let [soup (Jsoup/parse whole_html)
+        wishlist_items (.select soup "li.g-item-sortable")]
+    (mapv get_data wishlist_items)))
+
+
+(println (load_book_data_from_wishlist (slurp "whole_html.html")))
+
+
 
