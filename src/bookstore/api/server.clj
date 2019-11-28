dd66d1d496fd3737d11e3168d9fadbefda5a0eb0
Dominik Berger <dmnkbrgr@gmail.com>
2019-11-28 18:44:32 +0100

    add a working call to the mongoDB via the REST API

--- src/bookstore/api/server.clj
+++ src/bookstore/api/server.clj
@@ -1,12 +1,82 @@
 (ns bookstore.api.server
   (:require [compojure.api.sweet :refer :all]
-           [ring.util.http-response :refer :all]))
+            [schema.core :as s]
+            [ring.util.http-response :refer :all]
+            [bookstore.model :as bookstore]))
 
 ; to run local server on port 3000:
 ; lein ring server
 
+;(def app
+;  (api
+;    (GET "/hello" []
+;      :query-params [name :- String]
+;      (ok {:message (str "Hello, " name)}))))
+
+(s/defschema Pizza
+  {:name s/Str
+   (s/optional-key :description) s/Str
+   :size (s/enum :L :M :S)
+   :origin {:country (s/enum :FI :PO)
+            :city s/Str}})
+
+(s/defschema Money
+  {:amount s/Num
+   (s/optional-key :currency) s/Str})
+
+(s/defschema BookPrice
+  {:price Money
+   (s/optional-key :date) s/Str})
+
+(s/defschema Book
+  {:title s/Str
+   (s/optional-key :_id) s/Int
+   (s/optional-key :authors) [s/Str]
+   (s/optional-key :description) s/Str
+   (s/optional-key :length) s/Int
+   (s/optional-key :amazon-id) s/Int
+   (s/optional-key :amazon-url) s/Str
+   (s/optional-key :amazon-thumbnail-url) s/Str
+   (s/optional-key :amazon-date-added-to-wishlist) s/Str
+   (s/optional-key :amazon-price) BookPrice})
+
+(s/defschema Books [Book])
+
+
 (def app
   (api
-    (GET "/hello" []
-      :query-params [name :- String]
-      (ok {:message (str "Hello, " name)}))))
+    {:swagger
+     {:ui "/api-docs"
+      :spec "/swagger.json"
+      :data {:info {:title "Sample API"
+                    :description "Compojure Api example"}
+             :tags [{:name "api", :description "some apis"}]
+             :consumes ["application/json"]
+             :produces ["application/json"]}}}
+
+    (context "/api" []
+      :tags ["api"]
+
+      (GET "/plus" []
+        :return {:result Long}
+        :query-params [x :- Long, y :- Long]
+        :summary "adds two numbers together"
+        (ok {:result (+ x y)}))
+
+      (GET "/books" []
+        :return s/Any                                       ; Books
+        :summary "returns all books that are in the DB currently"
+        (ok
+          {:result bookstore/all_books}
+          ; TODO: I think for the above to work, each book needs to be transformed into Book schema
+          ))
+
+      (POST "/echo" []
+        :return Pizza
+        :body [pizza Pizza]
+        :summary "echoes a Pizza"
+        (ok pizza))
+
+      (GET "/hello" []
+        :query-params [name :- String]
+        (ok {:message (str "Hello, " name)})))))
--- src/bookstore/model.clj
+++ src/bookstore/model.clj
@@ -1,6 +1,8 @@
 (ns bookstore.model
   (:require [monger.core :as mg]
-            [monger.collection :as mc]))
+            [monger.collection :as mc])
+  ;(:require [validateur.validation :refer :all])
+  )
 
 ; To start MongoDB:
 ; mongod --dbpath=./resources/bookstore
@@ -14,15 +16,27 @@
         db (mg/get-db connection db_name)]
     (mc/insert-and-return db "books" book)))
 
+(defn stringify_id [entry]
+  (assoc entry :_id (str (get entry :_id))))
 
-(insert_new_book {
-   :amazon-id "asdf"
-   :title "Buch Sowieso"
-   :amazon-url "www.amazonas.br"
-   :authors ["Autor 1", "Autor 2"]
-   :amazon-thumbnail-url "www.amasdfa.sdfwe"
-   :amazon-date-added-to-wishlist "date representation!? -> Joda!"
-   :amazon-price {:date "date sowieso" :price {:amount 5.02 :currency "EUR"}}
-   ; TODO: Beschreibung, Veroeffentlichungsdatum, Verlag, user-generated tags, book language,
-   ;  original book language, length of book
-   })
+(def all_books
+  (let [connection (mg/connect)
+        db (mg/get-db connection db_name)
+        mongo_entries (mc/find-maps db "books")]
+    (map (partial stringify_id) mongo_entries)))
+
+
+;(insert_new_book {
+;   :amazon-id "asdf"
+;   :title "Buch Sowieso"
+;   :amazon-url "www.amazonas.br"
+;   :authors ["Autor 1", "Autor 2"]
+;   :amazon-thumbnail-url "www.amasdfa.sdfwe"
+;   :amazon-date-added-to-wishlist "date representation!? -> Joda!"
+;   :amazon-price {:date "date sowieso" :price {:amount 5.02 :currency "EUR"}}
+;   ; TODO: Beschreibung, Veroeffentlichungsdatum, Verlag, user-generated tags, book language,
+;   ;  original book language, length of book
+;   })
+
+(defn -main []
+  (println all_books))
