752010137cca8c9c590b8d5850eb917725993d85
Dominik Berger <dmnkbrgr@gmail.com>
2020-04-09 23:19:51 +0200

    add a GET call to the ring server for an arbitrary image, as an example

--- src/bookstore/api/server.clj
+++ src/bookstore/api/server.clj
@@ -14,31 +14,31 @@
 ;      (ok {:message (str "Hello, " name)}))))
 
 (s/defschema Pizza
-  {:name s/Str
+  {:name                         s/Str
    (s/optional-key :description) s/Str
-   :size (s/enum :L :M :S)
-   :origin {:country (s/enum :FI :PO)
-            :city s/Str}})
+   :size                         (s/enum :L :M :S)
+   :origin                       {:country (s/enum :FI :PO)
+                                  :city    s/Str}})
 
 (s/defschema Money
-  {:amount s/Num
+  {:amount                    s/Num
    (s/optional-key :currency) s/Str})
 
 (s/defschema BookPrice
-  {:price Money
+  {:price                 Money
    (s/optional-key :date) s/Str})
 
 (s/defschema Book
-  {:title s/Str
-   (s/optional-key :_id) s/Int
-   (s/optional-key :authors) [s/Str]
-   (s/optional-key :description) s/Str
-   (s/optional-key :length) s/Int
-   (s/optional-key :amazon-id) s/Int
-   (s/optional-key :amazon-url) s/Str
-   (s/optional-key :amazon-thumbnail-url) s/Str
+  {:title                                          s/Str
+   (s/optional-key :_id)                           s/Int
+   (s/optional-key :authors)                       [s/Str]
+   (s/optional-key :description)                   s/Str
+   (s/optional-key :length)                        s/Int
+   (s/optional-key :amazon-id)                     s/Int
+   (s/optional-key :amazon-url)                    s/Str
+   (s/optional-key :amazon-thumbnail-url)          s/Str
    (s/optional-key :amazon-date-added-to-wishlist) s/Str
-   (s/optional-key :amazon-price) BookPrice})
+   (s/optional-key :amazon-price)                  BookPrice})
 
 (s/defschema Books [Book])
 
@@ -46,11 +46,11 @@
 (def app
   (api
     {:swagger
-     {:ui "/api-docs"
+     {:ui   "/api-docs"
       :spec "/swagger.json"
-      :data {:info {:title "Sample API"
-                    :description "Compojure Api example"}
-             :tags [{:name "api", :description "some apis"}]
+      :data {:info     {:title       "Sample API"
+                        :description "Compojure Api example"}
+             :tags     [{:name "api", :description "some apis"}]
              :consumes ["application/json"]
              :produces ["application/json"]}}}
 
@@ -67,7 +67,7 @@
         :return s/Any                                       ; Books
         :summary "returns all books that are in the DB currently"
         (ok
-          {:result bookstore/all_books}
+          {:result bookstore/all-books}
           ; TODO: I think for the above to work, each book needs to be transformed into Book schema
           ))
 
@@ -79,4 +79,12 @@
 
       (GET "/hello" []
         :query-params [name :- String]
-        (ok {:message (str "Hello, " name)})))))
+        (ok {:message (str "Hello, " name)}))
+
+      (GET "/file" []
+        :summary "file download"
+        ;:return File
+        :produces ["image/jpeg"]
+        (->
+            (file-response "img1.jpg" {:root "resources/public"})
+            (header "Content-Type" "image/jpg"))))))
