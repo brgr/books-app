47d08e40fc51adacafabed778eb522c2de8efed6
Dominik Berger <dmnkbrgr@gmail.com>
2020-09-11 16:56:20 +0200

    purposefully select the physical copy of the book instead of just switching away from the Kindle version
    Before this, it could have switched to Audible. Now, it reads the text and looks to select one of the given acceptable book formats.

--- src/main/clj/amazon/books/fetch/single_book.clj
+++ src/main/clj/amazon/books/fetch/single_book.clj
@@ -2,6 +2,25 @@
   (:require [clojure.string :as str]
             [etaoin.api :refer :all]))
 
+(defn- scroll-to-when-exists [driver book-to-select]
+  (try (scroll-query driver [{:id :twister}
+                             {:tag :span :fn/text book-to-select}])
+       true
+       (catch Exception _
+         false)))
+
+(defn- select-physical-book [driver]
+  (let [physical-book (loop [books-to-try ["Broschiert" "Taschenbuch" "Gebundenes Buch"]]
+                        (if (scroll-to-when-exists driver (first books-to-try))
+                          (first books-to-try)
+                          (if (empty? (rest books-to-try))
+                            nil
+                            (recur (rest books-to-try)))))]
+    (click driver [{:id :twister}
+                   {:tag :span :fn/text physical-book}])))
+
+
+
 (defn- go-to-single-book [driver single-book-url]
   (go driver single-book-url)
   (wait driver 1)
@@ -9,9 +28,9 @@
     (click driver {:id :showMoreFormatsPrompt}))
   (let [text-of-selected-swatch (get-element-text driver [{:id :twister}
                                                           {:tag :div :fn/has-classes [:top-level :selected-row]}])]
-    (when (str/includes? text-of-selected-swatch "Kindle")
-      (click driver [{:id :twister}
-                     {:tag :div :fn/has-classes [:top-level :unselected-row]}]))))
+    (when (or (str/includes? text-of-selected-swatch "Kindle")
+              (str/includes? text-of-selected-swatch "Audible"))
+      (select-physical-book driver))))
 
 (defn get-single-book-html [single-book-url headless?]
   (let [driver (firefox {:headless headless?})]
