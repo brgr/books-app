3ee9d1e499bfbe5f01f1c0b26f8bff44c45fc8af
Dominik Berger <dmnkbrgr@gmail.com>
2020-09-24 21:35:05 +0200

    add a check to not switch book format if the current format is already OK

--- src/main/clj/amazon/books/fetch/single_book.clj
+++ src/main/clj/amazon/books/fetch/single_book.clj
@@ -2,6 +2,8 @@
   (:require [clojure.string :as str]
             [etaoin.api :refer :all]))
 
+(def accepted-formats ["Broschiert" "Taschenbuch" "Gebundenes Buch"])
+
 (defn- scroll-to-when-exists [driver book-to-select]
   (try (scroll-query driver [{:id :twister}
                              {:tag :span :fn/text book-to-select}])
@@ -10,7 +12,7 @@
          false)))
 
 (defn- select-physical-book [driver]
-  (let [physical-book (loop [books-to-try ["Broschiert" "Taschenbuch" "Gebundenes Buch"]]
+  (let [physical-book (loop [books-to-try accepted-formats]
                         (if (scroll-to-when-exists driver (first books-to-try))
                           (first books-to-try)
                           (if (empty? (rest books-to-try))
@@ -21,9 +23,7 @@
 
 
 
-(defn- go-to-single-book [driver single-book-url]
-  (go driver single-book-url)
-  (wait driver 1)
+(defn- switch-book-format [driver]
   (when (visible? driver {:id :showMoreFormatsPrompt})
     (click driver {:id :showMoreFormatsPrompt}))
   (let [text-of-selected-swatch (get-element-text driver [{:id :twister}
@@ -32,10 +32,22 @@
               (str/includes? text-of-selected-swatch "Audible"))
       (select-physical-book driver))))
 
+(defn current-format-selected [driver]
+  (get-element-text driver [{:id :tmmSwatches}
+                            ; The following is a workaround, because otherwise "unselected" would be found as well
+                            "//li[contains(concat(' ',normalize-space(@class),' '),' selected ')]"]))
+
+(defn str-contains-any? [s l]
+  (->> (map #(str/includes? s %) l)
+       (some true?)))
+
 (defn get-single-book-html [single-book-url headless?]
   (let [driver (firefox {:headless headless?})]
     (try
-      (go-to-single-book driver single-book-url)
+      (go driver single-book-url)
+      (wait driver 1)
+      (when (not (str-contains-any? (current-format-selected driver) accepted-formats))
+        (switch-book-format driver))
       ; This wait is needed for the iframe to appear always (otherwise it fails sometimes)
       (wait driver 2)
       (let [final-url (get-url driver)
