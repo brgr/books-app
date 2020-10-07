b8beec8f6adffb8c50c29e9f74f699699db638c3
Dominik Berger <dmnkbrgr@gmail.com>
2020-10-07 21:48:17 +0200

    add file paths for scraping to environ & add a local template file to the repo

--- profiles.clj
+++ profiles.clj
@@ -1,5 +1,12 @@
 {:dev
- {:env {:database-url "0.0.0.0"
-        :thumbnails-dir "src/main/resources/public/img/thumbnails/"
-        :front-matter-dir "src/main/resources/public/img/front_matter/"}}
+ {:env {:database-url     "0.0.0.0"
+        :thumbnails-dir   "src/main/resources/public/img/thumbnails/"
+        :front-matter-dir "src/main/resources/public/img/front_matter/"
+
+        :user-agents-file "src/main/resources/useragents.txt"
+        ; the following need to be set up like this on the machine the code is running on
+        :firefox-profile-prefs-template-file "src/main/resources/prefs-template.js"
+        :firefox-profile-directory "/home/dominik/.mozilla/firefox/djhmwgxs.useragent-switcher/"
+        ; this is just a normal firefox browser, but since it is a separate installation it was renamed
+        :firefox-browser-name "firefox-gecko"}}
  }
--- src/main/clj/amazon/books/fetch/scraping/driver.clj
+++ src/main/clj/amazon/books/fetch/scraping/driver.clj
@@ -1,16 +1,15 @@
 (ns amazon.books.fetch.scraping.driver
   (:require [etaoin.api :refer [chrome firefox-headless firefox go with-driver get-source quit get-logs]]
-            [clojure.string :as str]))
+            [clojure.string :as str]
+            [environ.core :refer [env]]))
 
 ; The useragents were originally taken from this site: (only the Browser user agents)
 ; https://techpatterns.com/downloads/firefox/useragentswitcher.xml
-(def user-agents (-> (slurp "src/main/resources/useragents.txt")
+(def user-agents (-> (slurp (env :user-agents-file))
                      (str/split #"\n")))
 
-; Fixme: Move this to environ!
-(def prefs-file-template "/home/dominik/.mozilla/firefox/hhib2u6i.default-release-1/prefs.js")
-(def profile-dir "/home/dominik/.mozilla/firefox/djhmwgxs.useragent-switcher")
-(def profile-prefs-file "/home/dominik/.mozilla/firefox/djhmwgxs.useragent-switcher/prefs.js")
+(def prefs-file-template (env :firefox-profile-prefs-template-file))
+(def profile-prefs-file (str (env :firefox-profile-directory) "prefs.js"))
 
 (defn add-preference [preference value]
   (spit profile-prefs-file
@@ -24,7 +23,6 @@
         (clojure.string/join $)))
 
 (defn get-driver [headless?]
-  ; Fixme: Save this as a template in my resources, then use that instead of the file from the Firefox that I currently use
   (->> (slurp prefs-file-template)
        (spit profile-prefs-file))
   (remove-preference "useragent")
@@ -34,5 +32,4 @@
             ; Note that we use another browser here (not the main installation of firefox)
             ; This other Firefox installation has the "useragent-switcher" profile set per default, which is then
             ; everytime updated
-            ; Fixme: Probably put the following name also into environ
-            :capabilities {:browserName "firefox-gecko"}}))
+            :capabilities {:browserName (env :firefox-browser-name)}}))
