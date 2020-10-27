5c633029f36580bbf029f548e6618a3a9e8691ac
Dominik Berger <dmnkbrgr@gmail.com>
2020-10-27 21:55:32 +0100

    add the book front matters for the books that are currently used for dev purposes

--- mongo/src/main/clj/quick_actions.clj
+++ mongo/src/main/clj/quick_actions.clj
@@ -6,9 +6,10 @@
 
 (comment
   (def books
-    (read-string (slurp "mongo/resources/books_template.clj")))
+    (read-string (slurp "mongo/src/main/resources/books_template.clj")))
   (-> books)
   (db/insert-new-books books))
+
 
 (comment
 
@@ -38,4 +39,35 @@
   (->> really-faulty-books
        (map :books.book/title)
        (filter #(clojure.string/includes? % "Versprechen")))
+
+
+  ; === images ===
+  ; the code below is for checking/moving the images
+
+  (def images-from-loaded-books
+    (->> books
+         (map :books.book/amazon-book-image-front)
+         (filter not-empty)
+         (map #(subs % (+ 1 (clojure.string/last-index-of % "/"))))
+         set))
+
+  (map #(clojure.java.io/copy %1 %2)
+    (->> images-from-loaded-books
+         (map #(clojure.java.io/file (str "src/main/resources/public/img/front_matter/" %))))
+    (->> images-from-loaded-books
+         (map #(clojure.java.io/file (str "mongo/src/main/resources/front_matters/" %)))))
+  ; todo: they are added now - use Git LFS to add them to git
+
+  (def all-loaded-images
+    (->> (file-seq (clojure.java.io/file "src/main/resources/public/img/front_matter"))
+         rest
+         (map str)
+         (map #(subs % (+ 1 (clojure.string/last-index-of % "/"))))
+         set))
+
+  (->> (clojure.set/difference all-loaded-images images-from-loaded-books)
+       count)
+
+  ; todo: there are more images loaded! are the ones from faulty probably loaded as well?
+  ;  --> if so, load them and add them! :-)
   )
