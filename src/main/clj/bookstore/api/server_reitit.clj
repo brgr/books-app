7d1883ecbead63cc7d894d5bec9de0e133068936
Dominik Berger <dominik.berger@protonmail.com>
2021-05-06 23:12:28 +0200

    fix CORS middleware by using regexes instead of strings -> This finally fixed the ClassCastException!
    I also changed the middleware in general a little bit. It is now mostly copied from an official reitit example

--- src/main/clj/bookstore/api/server_reitit.clj
+++ src/main/clj/bookstore/api/server_reitit.clj
@@ -78,27 +78,68 @@
       ;  ;                       {:status 200
       ;  ;                        :body   {:total (+ x y)}})}}]]]
       ;
-      {:data {:coercion   reitit.coercion.spec/coercion
-              :muuntaja   m/instance
-              ; todo: I am not sure which of these are actually needed... look them up, maybe I can remove some
-              :middleware [parameters/parameters-middleware
-                           muuntaja/format-negotiate-middleware
-                           muuntaja/format-response-middleware
-                           exception/exception-middleware
-                           muuntaja/format-request-middleware
-                           coercion/coerce-response-middleware
-                           coercion/coerce-request-middleware
-                           coercion/coerce-exceptions-middleware
-
-                           multipart/multipart-middleware
-                           [wrap-cors
-                            :access-control-allow-origin ["http://localhost:8280"]
-                            :access-control-allow-headers ["Origin" "X-Requested-With"
-                                                           "Content-Type" "Accept"]
-                            :access-control-allow-methods [:get :put :post :delete :options]]]}
-       ;:validate reitit.spec/validate
-       :exception reitit.dev.pretty/exception}
+      {;;:reitit.middleware/transform dev/print-request-diffs ;; pretty diffs
+       ;;:validate spec/validate ;; enable spec validation for route data
+       ;;:reitit.spec/wrap spell/closed ;; strict top-level validation
+       :exception reitit.dev.pretty/exception
+       :data      {:coercion   reitit.coercion.spec/coercion
+                   :muuntaja   m/instance
+                   :middleware [;; swagger feature
+                                swagger/swagger-feature
+                                ;; query-params & form-params
+                                parameters/parameters-middleware
+                                ;; content-negotiation
+                                muuntaja/format-negotiate-middleware
+                                ;; encoding response body
+                                muuntaja/format-response-middleware
+                                ;; exception handling
+                                exception/exception-middleware
+                                ;; decoding request body
+                                muuntaja/format-request-middleware
+                                ;; coercing response bodys
+                                coercion/coerce-response-middleware
+                                ;; coercing request parameters
+                                coercion/coerce-request-middleware
+                                ;; multipart
+                                multipart/multipart-middleware
+                                [wrap-cors
+                                 ; Note: The whole problem was because the following string is a regex - not a string!
+                                 :access-control-allow-origin [#"http://localhost:8280"]
+                                 ;:access-control-allow-headers ["*"
+                                                                ;"Accept"
+                                                                ;"Accept-Encoding"
+                                                                ;"Accept-Language"
+                                                                ;"Cache-Control"
+                                                                ;"Content-Length"
+                                                                ;"Connection"
+                                                                ;"Origin"
+                                                                ;"X-Requested-With"
+                                                                ;"Content-Type"
+                                                                ;"User-Agent"
+                                                                ;"Host"
+                                                                ;"Referer"
+                                                                ;"DNT"
+                                                                ;"Cookie"
+                                                                ;"Pragma"
+                                                                ;]
+                                 :access-control-allow-methods [:get :put :post :delete :options]]]}}
       )
+    
+;    "POST /book HTTP/1.1
+;Host: localhost:3000
+;User-Agent: Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:87.0) Gecko/20100101 Firefox/87.0
+;Accept: application/json
+;Accept-Language: en-US,en;q=0.5
+;Accept-Encoding: gzip, deflate
+;Referer: http://localhost:3000/index.html
+;Content-Type: application/json
+;Origin: http://localhost:3000
+;Content-Length: 23
+;DNT: 1
+;Connection: keep-alive
+;Cookie: JSESSIONID=E7EW8PFON8EAAV7aliAtFuvnKgi9XCeZ8UIkw7it
+;Pragma: no-cache
+;Cache-Control: no-cache"
 
     (ring/routes
       (swagger-ui/create-swagger-ui-handler
