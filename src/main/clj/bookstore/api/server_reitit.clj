c627222737dffed75ad56b6c73955fb4c49a1a46
Dominik Berger <dominik.berger@protonmail.com>
2021-05-08 20:01:54 +0200

    remove outdated comments in server_reitit.clj

--- src/main/clj/bookstore/api/server_reitit.clj
+++ src/main/clj/bookstore/api/server_reitit.clj
@@ -1,20 +1,20 @@
 (ns bookstore.api.server-reitit
-  (:require [environ.core :as env]
-            [reitit.ring :as ring]
-            [reitit.dev.pretty]
-            [reitit.coercion.schema]
-            [reitit.swagger :as swagger]
-            [ring.middleware.cors :refer [wrap-cors]]
-            [reitit.swagger-ui :as swagger-ui]
-            [muuntaja.core :as m]
-            [reitit.ring.middleware.muuntaja :as muuntaja]
-            [reitit.ring.middleware.exception :as exception]
-            [reitit.ring.middleware.multipart :as multipart]
-            [reitit.ring.middleware.parameters :as parameters]
-            [reitit.ring.coercion :as coercion]
-            [ring.adapter.jetty :as jetty]
+  (:require
+    [reitit.ring :as ring]
+    [reitit.dev.pretty]
+    [reitit.coercion.schema]
+    [reitit.swagger :as swagger]
+    [ring.middleware.cors :refer [wrap-cors]]
+    [reitit.swagger-ui :as swagger-ui]
+    [muuntaja.core :as m]
+    [reitit.ring.middleware.muuntaja :as muuntaja]
+    [reitit.ring.middleware.exception :as exception]
+    [reitit.ring.middleware.multipart :as multipart]
+    [reitit.ring.middleware.parameters :as parameters]
+    [reitit.ring.coercion :as coercion]
+    [ring.adapter.jetty :as jetty]
 
-            [bookstore.api.contexts.books :refer [books]]))
+    [bookstore.api.contexts.books :refer [books]]))
 
 (def app
   (ring/ring-handler
@@ -29,26 +29,6 @@
                                     :description "API for managing meta-data on books"}}
                :handler (swagger/create-swagger-handler)}}]]
 
-      ; TODO: Slowly move books + amazon-import API calls from using compojure-api to reitit
-      ; Note that the calls below are just here for reference, they can be removed after migration
-
-      ; ["/math"
-      ;  {:swagger {:tags ["math"]}}
-      ;
-      ;  ;["/plus"
-      ;  ; {:get  {:summary    "plus with spec query parameters"
-      ;  ;         :parameters {:query {:x int?, :y int?}}
-      ;  ;         :responses  {200 {:body {:total int?}}}
-      ;  ;         :handler    (fn [{{{:keys [x y]} :query} :parameters}]
-      ;  ;                       {:status 200
-      ;  ;                        :body   {:total (+ x y)}})}
-      ;  ;  :post {:summary    "plus with spec body parameters"
-      ;  ;         :parameters {:body {:x int?, :y int?}}
-      ;  ;         :responses  {200 {:body {:total int?}}}
-      ;  ;         :handler    (fn [{{{:keys [x y]} :body} :parameters}]
-      ;  ;                       {:status 200
-      ;  ;                        :body   {:total (+ x y)}})}}]]]
-      ;
       {;;:reitit.middleware/transform dev/print-request-diffs ;; pretty diffs
        ;;:validate spec/validate ;; enable spec validation for route data
        ;;:reitit.spec/wrap spell/closed ;; strict top-level validation
@@ -76,29 +56,12 @@
                                 [wrap-cors
                                  ; Note: The whole problem was because the following string is a regex - not a string!
                                  :access-control-allow-origin [#"http://localhost:8280"]
-                                 ;:access-control-allow-headers ["*"
-                                                                ;"Accept"
-                                                                ;"Accept-Encoding"
-                                                                ;"Accept-Language"
-                                                                ;"Cache-Control"
-                                                                ;"Content-Length"
-                                                                ;"Connection"
-                                                                ;"Origin"
-                                                                ;"X-Requested-With"
-                                                                ;"Content-Type"
-                                                                ;"User-Agent"
-                                                                ;"Host"
-                                                                ;"Referer"
-                                                                ;"DNT"
-                                                                ;"Cookie"
-                                                                ;"Pragma"
-                                                                ;]
                                  :access-control-allow-methods [:get :put :post :delete :options]]]}})
-    
+
     (ring/routes
       (swagger-ui/create-swagger-ui-handler
-        {:path "/"
-         :config {:validatorUrl nil
+        {:path   "/"
+         :config {:validatorUrl     nil
                   :operationsSorter "alpha"}})
       (ring/create-default-handler
         ;{:not-found (constantly {:status 404 :body "Not found"})}
