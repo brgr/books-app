8dce370eed0c92308570cfc9109e27c0c77607a4
Dominik Berger <dominik.berger@protonmail.com>
2021-05-08 19:28:58 +0200

    migrate DELETE book call from compojure-api to reitit + rearrange API endpoints to be more RESTy

--- src/main/clj/bookstore/api/contexts/books.clj
+++ src/main/clj/bookstore/api/contexts/books.clj
@@ -10,19 +10,21 @@
             [bookstore.files.file-management :refer [get-file-name]]))
 
 (def books
-  ["/books"
-   {:swagger {:tags ["books"]}
-    }
+  [
+   ["/books"
+    ; todo: use a Schema to specify what is returned!
+    {:swagger {:tags ["books"]}
+     :get {:summary "Returns all books that are in the DB currently"
+           :status  200
+           ;:no-doc true
+           :handler (fn [_] {:status 200
+                             :body   (bookstore/all-books)})}}]
 
-   [["/"
-     ; todo: use a Schema to specify what is returned!
-     {:get {:summary "Returns all books that are in the DB currently"
-            :status  200
-            ;:no-doc true
-            :handler (fn [_] {:status 200
-                              :body   (bookstore/all-books)})}}]
+   ["/book"
+    {:swagger {:tags ["book"]}
+     }
 
-    ["/book"
+    ["/"
      {:post {:summary    "Insert a new book"
              :parameters {:body s/Any}                      ; todo: check that book is of correct type!
              ;:responses  {200 {:body s/Any}} ; is this correct like this?
@@ -31,35 +33,37 @@
                              {:status 200
                               :body   inserted-book}))}}]
 
-    ; todo : change the underscore to a dash
-    ["/:book-id/front_matter"
-     {:get {:summary    "Given a book id, returns a jpg picture of its front matter"
-            :parameters {:query {:book-id s/Str}}
-            :swagger    {:produces ["image/jpg"]}
-            ; fixme: handle case where there is no image! (what is happening now, in that case?)
-            :handler    (fn [{{{:keys [book-id]} :query} :parameters}]
-                          {:status  200
-                           :headers {"Content-Type" "image/png"}
-                           :body    (when-let [image-url (:amazon-book-image-front (bookstore.db.model/get-book-by-id book-id))]
-                                      (io/input-stream
-                                        (io/resource (str (:front-matter-dir env) (get-file-name image-url)))))})}}]
+    ["/:book-id"
 
+     ["/"
+      {:delete {:summary    "Given a books id, delete this book from the database"
+                :parameters {:query {:book-id s/Str}}
+                ;:responses  {200 {:body s/Any}} ; is this correct like this?
+                :handler    (fn [{{{:keys [book-id]} :query} :parameters}]
+                              (let [write-result (bookstore/remove-book-by-id book-id)
+                                    count-removed-books (.getN write-result)]
+                                (println "write result:" write-result)
+                                (if (= 1 count-removed-books)
+                                  {:status 200
+                                   :body   {:id book-id}}
+                                  {:status 404
+                                   :body   {:id book-id}})))}}]
+
+     ["/front-matter"
+      {:get {:summary    "Given a book id, returns a jpg picture of its front matter"
+             :parameters {:query {:book-id s/Str}}
+             :swagger    {:produces ["image/jpg"]}
+             ; fixme: handle case where there is no image! (what is happening now, in that case?)
+             :handler    (fn [{{{:keys [book-id]} :query} :parameters}]
+                           {:status  200
+                            :headers {"Content-Type" "image/png"}
+                            :body    (when-let [image-url (:amazon-book-image-front (bookstore.db.model/get-book-by-id book-id))]
+                                       (io/input-stream
+                                         (io/resource (str (:front-matter-dir env) (get-file-name image-url)))))})}}]
+     ]
     ]
    ]
 
-
-  ; TODO: Migrate other calls below to Reitit as well
-
-
-  ;
-  ;(DELETE "/book" []
-  ;  :summary "Delete book by given ID"
-  ;  :query-params [id :- String]
-  ;  (let [write-result (bookstore/remove-book-by-id id)
-  ;        count-removed-books (.getN write-result)]
-  ;    (if (= 1 count-removed-books)
-  ;      (ok {:id id})
-  ;      (not-found {:id id}))))
   ;
   ;(DELETE "/books/all" []
   ;  :summary "Delete all books in the database."
