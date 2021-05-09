479a6e7255a06ade00fe6f328921114f8e04e46e
Dominik Berger <dominik.berger@protonmail.com>
2021-05-09 21:43:51 +0200

    clean up amazon_search.clj and authors.clj; start testing it manually

--- src/main/clj/amazon/search/amazon_search.clj
+++ src/main/clj/amazon/search/amazon_search.clj
@@ -1,35 +1,61 @@
 (ns amazon.search.amazon-search
   (:require
-    [amazon.search.parse.authors :refer [get-authors-from-metadata-below-title]])
+    [amazon.search.parse.authors :refer [get-authors-from-metadata-below-title]]
+    [schema.core :as s])
   (:import
     [org.jsoup Jsoup]
-    (java.net URL)))
+    (java.net URL)
+    (org.jsoup.select Elements)
+    (org.jsoup.nodes Document Element)))
 
 (def url "https://www.amazon.de/s?k=the+beauty+of+everyday+things")
 
 (def html (Jsoup/parse (URL. url) 5000))
 
-(def first-item (-> (.select html "div.s-result-item[data-component-type=s-search-result]")
-                    (first)))
+(s/defn get-results :- Elements
+  [soup :- Document]
+  (.select soup "div.s-result-item[data-component-type=s-search-result]"))
 
-; get the thumbnail
-(->> (.select first-item "img")
-     (map #(.attr % "src")))
+(get-results html)
 
-; get the URL to the product
-(-> (.select first-item "h2 > a")
-    (.attr "href"))
+(s/defn get-thumbnail-url :- String
+  [search-result :- Element]
+  (-> (.select search-result "img")
+      (first)
+      (.attr "src")))
 
-; get title
-(-> (.select first-item "h2 span")
-    (.text))
+(s/defn get-product-url-slug :- String
+  [search-result :- Element]
+  (-> (.select search-result "h2 > a")
+      (.attr "href")))
 
-; with the next 2: get authors
-(def metadata-below-title (-> (.select first-item "div.a-section > h2")
-                              (.parents)
-                              (first)
-                              (.select "div.a-color-secondary > div > span")))
+(s/defn get-title :- String
+  [search-result :- Element]
+  (-> (.select search-result "h2 span")
+      (.text)))
 
-(as-> (map #(.text %) metadata-below-title) data
-      (clojure.string/join " " data)
-      #_(get-authors-from-metadata-below-title data))
+(s/defn get-metadata-below-title :- String
+  [search-result :- Element]
+  (as-> (.select search-result "div.a-section > h2") data
+        (.parents data)
+        (first data)
+        (.select data "div.a-color-secondary > div > span")
+        (map #(.text %) data)
+        (clojure.string/join " " data)))
+
+(s/defn parse-search-result
+  [search-result :- Element]
+  {:title            (get-title search-result)
+   :product-url-slug (get-product-url-slug search-result)
+   :thumbnail-url    (get-thumbnail-url search-result)
+   :authors          (-> (get-metadata-below-title search-result)
+                         (get-authors-from-metadata-below-title))})
+
+(s/defn get-search-results
+  [search-results :- Elements]
+  (map parse-search-result search-results))
+
+(get-search-results (get-results html))
+; todo: manually check the results; compare with results on the rendered HTML!
+; problems:
+; 1. when author is a link
--- src/main/clj/amazon/search/parse/authors.clj
+++ src/main/clj/amazon/search/parse/authors.clj
@@ -5,9 +5,10 @@
 
 (s/defn get-authors-from-metadata-below-title :- [s/Str]
   [metadata-below-title :- s/Str]
-  (as-> (str/split metadata-below-title #"\|") data
-        (map str/trim data)
-        (filter #(str/starts-with? % "von ") data)
-        (first data)
-        (subs data 4)
-        (str/split data #" und ")))
+  (when-let [authors-plain-text (as-> (str/split metadata-below-title #"\|") data
+                                 (map str/trim data)
+                                 (filter #(str/starts-with? % "von ") data)
+                                 (first data))]
+
+    (-> (subs authors-plain-text 4)
+        (str/split #" und "))))
