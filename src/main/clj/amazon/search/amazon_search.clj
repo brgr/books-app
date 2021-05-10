be4051f4f94671c2617a21364b8bc2e6756a96f3
Dominik Berger <dominik.berger@protonmail.com>
2021-05-10 22:52:42 +0200

    in amazon_search.clj, move parsing of single search result items into their own file search_result.clj

--- src/main/clj/amazon/search/amazon_search.clj
+++ src/main/clj/amazon/search/amazon_search.clj
@@ -1,12 +1,13 @@
 (ns amazon.search.amazon-search
   (:require
     [amazon.search.parse.authors :refer [get-authors-from-metadata-below-title]]
+    [amazon.search.parse.search-result :refer [SearchResult parse-search-results]]
     [schema.core :as s])
   (:import
     [org.jsoup Jsoup]
     (java.net URL URLEncoder)
     (org.jsoup.select Elements)
-    (org.jsoup.nodes Document Element)))
+    (org.jsoup.nodes Document)))
 
 (defn build-amazon-search-url
   [search]
@@ -15,51 +16,6 @@
 (s/defn get-results :- Elements
   [soup :- Document]
   (.select soup "div.s-result-item[data-component-type=s-search-result]"))
-
-(s/defn get-thumbnail-url :- String
-  [search-result :- Element]
-  (-> (.select search-result "img")
-      (first)
-      (.attr "src")))
-
-(s/defn get-product-url-slug :- String
-  [search-result :- Element]
-  (-> (.select search-result "h2 > a")
-      (.attr "href")))
-
-(s/defn get-title :- String
-  [search-result :- Element]
-  (-> (.select search-result "h2 span")
-      (.text)))
-
-(s/defn get-metadata-below-title :- String
-  [search-result :- Element]
-  (as-> (.select search-result "div.a-section > h2") data
-        (.parents data)
-        (first data)
-        (.select data "div.a-color-secondary > div.a-row")
-        (first data)
-        (.children data)
-        (map #(.text %) data)
-        (clojure.string/join " " data)))
-
-(def SearchResult
-  {:title            String
-   :product-url-slug String
-   :thumbnail-url    String
-   :authors          [String]})
-
-(s/defn parse-search-result :- String
-  [search-result :- Element]
-  {:title            (get-title search-result)
-   :product-url-slug (get-product-url-slug search-result)
-   :thumbnail-url    (get-thumbnail-url search-result)
-   :authors          (-> (get-metadata-below-title search-result)
-                         (get-authors-from-metadata-below-title))})
-
-(s/defn parse-search-results :- [SearchResult]
-  [search-results :- Elements]
-  (map parse-search-result search-results))
 
 (s/defn find-results :- [SearchResult]
   [soup :- Jsoup]
