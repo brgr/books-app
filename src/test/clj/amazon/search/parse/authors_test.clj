fbd0ef4ffe94e6aca1b8a231356479d1bcfaac25
Dominik Berger <dominik.berger@protonmail.com>
2021-05-13 16:14:23 +0200

    remove a todo in authors_test.clj

--- src/test/clj/amazon/search/parse/authors_test.clj
+++ src/test/clj/amazon/search/parse/authors_test.clj
@@ -5,6 +5,3 @@
 (deftest get-authors-from-metadata-below-title-test
   (assert (= ["Soetsu Yanagi" "Michael Brase"]
              (get-authors-from-metadata-below-title "Englisch Ausgabe  |  von Soetsu Yanagi und Michael Brase  |  8. Oktober 2019"))))
-
-; todo: add more tests
-;  search for example simply for "book" in amazon.de and put all of these as a test!
