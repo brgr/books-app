aa3de03e53df5b7de45e32bdc16b2239f0f075a1
Dominik Berger <dominik.berger@protonmail.com>
2021-05-13 16:00:18 +0200

    slightly clean up search.amazon, adding a swagger description instead of just the summary

--- src/main/clj/bookstore/api/routes/search/amazon.clj
+++ src/main/clj/bookstore/api/routes/search/amazon.clj
@@ -4,14 +4,18 @@
     [amazon.search.parse.search-result :refer [SearchResult]]
     [amazon.search.amazon-search :refer [search-amazon]]))
 
+(def search-amazon-description
+  "Calls https://www.amazon.de/s?k={search-text} and parses the results. Also filters some of the results, if they
+  highly don't appear to be related to the results. It does however not filter only for books, so all kind of products
+  are returned.")
+
 (def amazon-search-routes
   ["/search/amazon"
    {:swagger {:tags ["search"]}}
 
    ["/"
-    {:get {:summary    "Search products on Amazon. Calls https://www.amazon.de/s?k={search-text} and parses the results.
-    Also filters some of the results, if they highly don't appear to be related to the results.
-    It does however not filter only for books, so all kind of products are returned."
+    {:get {:summary    "Search products on amazon.de"
+           :swagger    {:description search-amazon-description}
            :parameters {:query {:search-text s/Str}}
            :responses  {200 {:body {:result [SearchResult]}}}
            :handler    (fn [{{{:keys [search-text]} :query} :parameters}]
