b94c953a77449544a7f5d71433b922f135153fed
Dominik Berger <dominik.berger@protonmail.com>
2021-05-13 16:09:26 +0200

    reformat filter_search_result.clj

--- src/main/clj/amazon/search/filter/filter_search_result.clj
+++ src/main/clj/amazon/search/filter/filter_search_result.clj
@@ -11,6 +11,6 @@
   (clojure.string/starts-with? (:product-url-slug search-result) "/gp/slredirect/picassoRedirect.html"))
 
 (s/defn filter-shady-results :- [SearchResult]
-        "Filters strange results out. It seems that Amazon gives these out on purpose, but I'm not sure."
-        [original-search-results :- [SearchResult]]
-        (filter #(not (starts-url-slug-strangely? %)) original-search-results))
+  "Filters strange results out. It seems that Amazon gives these out on purpose, but I'm not sure."
+  [original-search-results :- [SearchResult]]
+  (filter #(not (starts-url-slug-strangely? %)) original-search-results))
