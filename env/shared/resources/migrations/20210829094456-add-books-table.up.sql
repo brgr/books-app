7cdbac6ae7de6b541ddc11be20cffa06015921bd
Dominik Berger <dominik.berger@protonmail.com>
2021-09-13 18:15:31 +0200

    fix 1st books migrations
    - use identity columns
    - fix isbn/asin
    
    #BOOKS-22

--- env/shared/resources/migrations/20210829094456-add-books-table.up.sql
+++ env/shared/resources/migrations/20210829094456-add-books-table.up.sql
@@ -1,34 +1,58 @@
 
+/******************
+ *   DATE UTILS   *
+ ******************/
+
 create table date_precision (
     precision text primary key
 );
 --;;
 
--- Note that this table is "built" like an enum, without needing to use that specifically. Enums cannot super easily
--- be changed, which is why we preferred this way. As there is no number id saved (only the text itself), a join will
--- also not be needed, as the text is directly saved at the row that needs it. This will of course need more storage,
--- as each row saves its value (instead of just an ID number that links to it), but I think that will for now not be
--- a problem.
--- I have this idea from here: https://stackoverflow.com/a/41655273/1564218
+/*
+ * Note that this table is "built" like an enum, without needing to use that
+ * specifically. Enums cannot super easily be changed, which is why we
+ * preferred this way. As there is no number id saved (only the text itself),
+ * a join will also not be needed, as the text is directly saved at the row
+ * that needs it. This will of course need more storage, as each row saves
+ * its value (instead of just an ID number that links to it), but I think
+ * that will for now not be a problem.
+ * I have this idea from here: https://stackoverflow.com/a/41655273/1564218
+ */
 insert into date_precision values
     ('year'),
     ('month'),
     ('day');
 --;;
 
--- This table is meant for several book *editions*. Currently, we won't link them to a single book. This can however be
--- added as a feature in the future.
+
+/************************
+ *   MAIN BOOKS TABLE   *
+ ************************/
+
+/*
+ * This table is meant for several book *variations*.
+ *
+ * The hierarchy goes like this: book / edition / variation.
+ * As an example: Don Quijote / German Translation from 2010 by some person / paperback
+ * Editions are the different "releases" of the book, variations are the different "channels" of this "release", for
+ * example paperbacks or ebooks.
+ *
+ * Currently, we don't save "editions" or "books", even though we could, and then we could link them together. This is
+ * at the moment not in scope though. (This is more in scope of a project like OpenLibrary, which really wants a great
+ * and open library - this project however wants a personal books library, and personally we will most likely only have
+ * a specific variation of a book)
+ */
 create table books (
-    id serial primary key,
+    id int primary key generated always as identity,
     title text not null,
     subtitle text,
 
-    -- An ISBN is given to each (edition, variation) tuple. The hierarchy goes like this: book / edition / variation.
-    -- As an example: Don Quijote / German Translation from 2010 by some person / paperback
-    -- Editions are the different "releases" of the book, variations are the different "channels" of this "release", for
-    -- example paperbacks or ebooks.
-    isbn_13 text[],
-    isbn_10 text[],
+    -- An ISBN is given to each variation
+    isbn_13 text,
+    isbn_10 text,
+    -- ASIN = Amazon Standard Identification Number; for books, this should be equal to ISBN10, but for example for
+    -- Kindle books this seems to not be the case (which is why this is a separate column here)
+    asin text,
 
     -- For now, we will keep "language" very easy, i.e. just a text for each language
     language text,
@@ -61,14 +85,19 @@ create table books (
     last_modified timestamp
 );
 --;;
+
+/***************
+ *   AUTHORS   *
+ ***************/
+
 create table authors (
-    id serial primary key,
+    id int primary key generated always as identity,
     full_name text not null unique,
     alternate_names text[]
 );
 --;;
 create table books_authors (
-    id serial primary key,
+    id int primary key generated always as identity,
     fk_book integer not null,
     fk_author integer not null,
 
@@ -80,16 +109,21 @@ create table books_authors (
         on update cascade
 );
 --;;
+
+/******************
+ *   PUBLISHERS   *
+ ******************/
+
 create table publishers (
-    id serial primary key,
+    id int primary key generated always as identity,
     full_name text not null unique,
     -- For example: full_name = Penguin, alternate_names = {Penguin Books, Penguin Science, ...}
     -- Note that I won't distinguish sub-publishers (e.g. Penguin Science) for now
     alternate_names text[]
-)
+);
 --;;
 create table books_publishers (
-    id serial primary key,
+    id int primary key generated always as identity,
     fk_book integer not null,
     fk_publisher integer not null,
 
@@ -99,8 +133,18 @@ create table books_publishers (
     foreign key (fk_publisher) references publishers (id)
         on delete cascade -- We might have a publisher duplicated, in which case we allow deletion
         on update restrict
-)
+);
+--;;
 
--- todo: genres table
--- todo: maybe: create a links table
--- todo: tags
+-- Todo: a table for the import sources, e.g. original (full) amazon link, original front cover link
+-- This is different than e.g. saving the amazon link, as that one can simply be generated from the ASIN, but here
+-- we would like to know the full source link, with all the parameters etc.
+-- --> Make a revisions table! The first revision is for example an automated import, after that we may make
+--     manual revisions, or scrape some other site via which we add data. We can have a notes column which logs the
+--     changes. The question would be if we need an exact diff of the objects, but I would say we don't. That remains
+--     to be thought about though.
+
+-- Todo: Genres table
+-- Todo: Maybe: create a links table
+-- --> this is problematic because the ASIN can already generate the amazon link, for example (therefore this would not be normalized)
+-- Todo: Tags
