7c1433539d26c271d3777e6a2b63c21821b72580
Dominik Berger <dominik.berger@protonmail.com>
2022-01-17 10:09:30 +0100

    Revert "WIP changing to PostgreSQL: configure a lot of stuff quite similar to luminus, which mainly comes from the fact that we now also use mount and cprops; besides those two libraries I have also integrated other useful stuff, e.g. some of the directory structure and some logging (though I am not sure how well that works already)"
    This reverts commit 3cb73227b6a3968484276e2395f2226387202e25.

--- env/dev/clj/books/dev_middleware.clj
+++
@@ -1,9 +0,0 @@
-(ns books.dev-middleware
-  (:require
-    [ring.middleware.reload :refer [wrap-reload]]
-    [prone.middleware :refer [wrap-exceptions]]))
-
-(defn wrap-dev [handler]
-  (-> handler
-      wrap-reload
-      (wrap-exceptions {:app-namespaces ['luminus-postgres-template]})))
